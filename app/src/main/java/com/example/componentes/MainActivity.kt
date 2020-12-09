d944c5b921f3ce99a644b443f4f75cd74268925e
rogerbernar9 <roger.bernar9@gmail.com>
2020-12-09 10:06:08 -0300

    aulas 212 e 213 - Spinner estático e dinâmico

--- app/src/main/java/com/example/componentes/MainActivity.kt
+++ app/src/main/java/com/example/componentes/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -17,8 +18,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         buttonToast.setOnClickListener(this)
-
         buttonSnack.setOnClickListener(this)
+
+        loadSpinner()
+    }
+
+    private fun loadSpinner() {
+        val mList = listOf("Gramas", "Kg", "Arroba", "Tonelada")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mList)
+        spinner_dynamic.adapter = adapter
     }
 
     override fun onClick(v: View) {
